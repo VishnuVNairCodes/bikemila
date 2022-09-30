3a99e83c01e0d4c800d3684dc775822244552926
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2022-09-30 12:53:37 +0530

    feat: added NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,34 @@
+import { Stack } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
+import { SearchBar } from "..";
 
-const NavBar = () => {
-  return <div>NavBar</div>;
-};
+import "./NavBar.css";
+
+const NavBar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    justifyContent="space-between"
+    p={2}
+    sx={{
+      position: "sticky",
+      top: 0,
+      backgroundColor: "#000",
+    }}
+  >
+    <Link to="/" className="nav-logo">
+      <img
+        src={require("../../assets/images/logo.png")}
+        alt="logo"
+        className="nav-logo-img"
+      />
+      <h1 className="nav-logo-title">
+        Bike<span>Mila</span>
+      </h1>
+    </Link>
+    <SearchBar />
+  </Stack>
+);
 
 export { NavBar };
