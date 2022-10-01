b65a3bc8b14aba9b004f666b151624b887f9a7e7
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2022-10-01 08:16:02 +0530

    feat: added sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Stack } from "@mui/material";
+import { categories } from "../../utils/data";
+import "./Sidebar.css";
+
+const selectedCategory = "New";
 
 const Sidebar = () => (
   <Stack
@@ -8,9 +12,19 @@ const Sidebar = () => (
       overflowY: "auto",
       height: { xs: "auto", md: "95%" },
       flexDirection: { md: "column" },
+      pl: { xs: 2, md: 0 },
     }}
   >
-    Sidebar
+    {categories.map(({ id, name }) => (
+      <button
+        key={id}
+        className={`category-btn ${
+          name === selectedCategory && "selected-category-btn"
+        }`}
+      >
+        {name}
+      </button>
+    ))}
   </Stack>
 );
 
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,16 +1,18 @@
+import { v4 as uuidv4 } from "uuid";
+
 const categories = [
-  "New",
-  "Tourer",
-  "ADV",
-  "Retro",
-  "Cruiser",
-  "Bobber",
-  "Cafe Racer",
-  "Scrambler",
-  "Naked Street",
-  "Electric",
-  "Superbike",
-  "Sports Tourer",
+  { id: uuidv4(), name: "New" },
+  { id: uuidv4(), name: "Tourer" },
+  { id: uuidv4(), name: "ADV" },
+  { id: uuidv4(), name: "Retro" },
+  { id: uuidv4(), name: "Cruiser" },
+  { id: uuidv4(), name: "Bobber" },
+  { id: uuidv4(), name: "Cafe Racer" },
+  { id: uuidv4(), name: "Scrambler" },
+  { id: uuidv4(), name: "Naked Street" },
+  { id: uuidv4(), name: "Electric" },
+  { id: uuidv4(), name: "Superbike" },
+  { id: uuidv4(), name: "Sports Tourer" },
 ];
 
 export { categories };
