757dfbf2e0b800d78c822d3837bb1110675fb11a
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2022-10-01 06:38:28 +0530

    feat: created data.js & added categories array

---
+++ src/utils/data.js
@@ -0,0 +1,16 @@
+const categories = [
+  "New",
+  "Tourer",
+  "ADV",
+  "Retro",
+  "Cruiser",
+  "Bobber",
+  "Cafe Racer",
+  "Scrambler",
+  "Naked Street",
+  "Electric",
+  "Superbike",
+  "Sports Tourer",
+];
+
+export { categories };
