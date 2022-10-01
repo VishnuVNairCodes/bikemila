51d9509990cf72d19450b9c2edc80c9ebc769078
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2022-10-01 06:36:47 +0530

    style: added styling to feed component

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Stack, Box } from "@mui/material";
+import { Sidebar } from "..";
 
-const Feed = () => {
-  return <div>Feed</div>;
-};
+const Feed = () => (
+  <Stack
+    sx={{
+      flexDirection: {
+        xs: "column",
+        md: "row",
+      },
+    }}
+  >
+    <Box
+      sx={{
+        height: {
+          xs: "auto",
+          md: "92vh",
+        },
+        borderRight: "1px solid #3d3d3d",
+        px: { xs: 0, md: 2 },
+      }}
+    >
+      <Sidebar />
+    </Box>
+  </Stack>
+);
 
 export { Feed };
