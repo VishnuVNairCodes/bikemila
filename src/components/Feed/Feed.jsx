00547c62ae77350a5e3ebfd2e20c56b9ca9e1f79
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2022-10-08 11:05:29 +0530

    feat: added feed

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Stack, Box, Typography } from "@mui/material";
+import { Sidebar, Videos } from "..";
+import { fetchFromAPI } from "../../utils/fetch-from-API";
+import "./Feed.css";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [selectedCategory, setSelectedCategory] = useState({
+    id: 0,
+    name: "New",
+    keywords: "New",
+  });
+  const [videos, setVideos] = useState([]);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const { items } = await fetchFromAPI(
+  //         `search?part=snippet&q=2022 ${selectedCategory.keywords} motorcycle bike review Powerdrift Zigwheels Strell Sagar Sheldekar Autocar Overdrive Faisal Khan`
+  //       );
+  //       setVideos(items);
+  //     } catch (error) {
+  //       console.error(error);
+  //       // replace this with proper error handling on the view
+  //     }
+  //   })();
+  // }, [selectedCategory]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { items } = await fetchFromAPI(`search?part=snippet&q=Zigwheels`);
+        setVideos(items);
+      } catch (error) {
+        console.error(error);
+        // replace this with proper error handling on the view
+      }
+    })();
+  }, [selectedCategory]);
+
+  return (
+    <Stack
+      sx={{
+        flexDirection: {
+          xs: "column",
+          md: "row",
+        },
+      }}
+    >
+      <Box
+        sx={{
+          borderRight: "1px solid var(--border-sidebar)",
+          px: { xs: 0, md: "1.6rem" },
+        }}
+      >
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+      </Box>
+      <Box p="1.6rem" sx={{ overflowY: "auto", flexGrow: 1 }}>
+        <Typography
+          className="feed-heading"
+          variant="h4"
+          fontWeight="bold"
+          gutterBottom
+        >
+          {selectedCategory.name} <span>bikes</span>
+        </Typography>
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
 };
 
 export { Feed };
