bfacab831c6fd931cfb7c68f9e24b99935dff7f7
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2022-10-08 11:04:43 +0530

    feat: added sidebar

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Stack } from "@mui/material";
+import { categories } from "../../utils/data";
+import "./Sidebar.css";
+
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
+  <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { xs: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+      pl: { xs: "1.6rem", md: 0 },
+    }}
+  >
+    {categories.map((category) => (
+      <button
+        key={category.id}
+        className={`category-btn ${
+          category.name === selectedCategory.name && "selected-category-btn"
+        }`}
+        onClick={() => setSelectedCategory(category)}
+      >
+        {category.name}
+      </button>
+    ))}
+  </Stack>
+);
+
+export { Sidebar };
