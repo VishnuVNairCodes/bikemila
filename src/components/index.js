cd5c9ab721b16f45b6333f398f281021cb09265a
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2022-10-08 11:07:13 +0530

    update exports from components folder

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,7 @@ export { VideoDetail } from "./VideoDetail/VideoDetail";
 export { ChannelDetail } from "./ChannelDetail/ChannelDetail";
 export { SearchFeed } from "./SearchFeed/SearchFeed";
 export { SearchBar } from "./SearchBar/SearchBar";
+export { Sidebar } from "./Sidebar/Sidebar";
+export { Videos } from "./Videos/Videos";
+export { VideoCard } from "./VideoCard/VideoCard";
+export { ChannelCard } from "./ChannelCard/ChannelCard";
