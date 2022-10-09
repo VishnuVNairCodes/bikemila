e2f9b9903687a4172b529c4e9f5795f0d66addd3
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2022-10-09 17:48:03 +0530

    fix: removed unused variables

--- src/components/VideoDetail/VideoDetail.jsx
+++ src/components/VideoDetail/VideoDetail.jsx
@@ -3,15 +3,12 @@ import { Link, useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
 import { Box, Stack, Typography } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
-
-import { Videos } from "..";
 import { fetchFromAPI } from "../../utils/fetch-from-API";
 
 const VideoDetail = () => {
   const { id } = useParams();
 
   const [videoDetail, setVideoDetail] = useState(null);
-  const [videos, setVideos] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -35,59 +32,47 @@ const VideoDetail = () => {
 
   return (
     <Box minHeight="95vh">
-      <Stack direction={{ xs: "column", md: "row" }}>
-        <Box flex={1}>
-          <Box sx={{ width: "100%" }}>
-            <ReactPlayer
-              url={`https://www.youtube.com/watch?v=${id}`}
-              controls
-              className="react-player"
-              height="77vh"
-              width="100%"
-            />
-            <Typography
-              color="var(--white)"
-              variant="h5"
-              fontWeight="bold"
-              p={"1.6rem"}
-            >
-              {title}
+      <Box sx={{ width: "100%" }}>
+        <ReactPlayer
+          url={`https://www.youtube.com/watch?v=${id}`}
+          controls
+          className="react-player"
+          height="77vh"
+          width="100%"
+        />
+        <Typography
+          color="var(--white)"
+          variant="h5"
+          fontWeight="bold"
+          p={"1.6rem"}
+        >
+          {title}
+        </Typography>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          sx={{ color: "var(--white)" }}
+          py="8px"
+          px="1.6rem"
+        >
+          <Link to={`/channel/${channelId}`} className="link">
+            <Typography variant="h6" color="var(--white)">
+              {channelTitle}
+              <CheckCircle
+                sx={{ fontSize: "1.2rem", color: "var(--gray)", ml: "5px" }}
+              />
             </Typography>
-            <Stack
-              direction="row"
-              justifyContent="space-between"
-              sx={{ color: "var(--white)" }}
-              py="8px"
-              px="1.6rem"
-            >
-              <Link to={`/channel/${channelId}`} className="link">
-                <Typography variant="h6" color="var(--white)">
-                  {channelTitle}
-                  <CheckCircle
-                    sx={{ fontSize: "1.2rem", color: "var(--gray)", ml: "5px" }}
-                  />
-                </Typography>
-              </Link>
-              <Stack direction="row" gap="2rem" alignItems="center">
-                <Typography variant="body1" sx={{ opacity: 0.7 }}>
-                  {parseInt(viewCount).toLocaleString()} views
-                </Typography>
-                <Typography variant="body1" sx={{ opacity: 0.7 }}>
-                  {parseInt(likeCount).toLocaleString()} likes
-                </Typography>
-              </Stack>
-            </Stack>
-          </Box>
-        </Box>
-      </Stack>
-      {/* <Box
-        px={"1.6rem"}
-        py={{ md: "8px", xs: "4rem" }}
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Videos videos={videos} />
-      </Box> */}
+          </Link>
+          <Stack direction="row" gap="2rem" alignItems="center">
+            <Typography variant="body1" sx={{ opacity: 0.7 }}>
+              {parseInt(viewCount).toLocaleString()} views
+            </Typography>
+            <Typography variant="body1" sx={{ opacity: 0.7 }}>
+              {parseInt(likeCount).toLocaleString()} likes
+            </Typography>
+          </Stack>
+        </Stack>
+      </Box>
     </Box>
   );
 };
